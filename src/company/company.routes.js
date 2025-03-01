0878a8a5b1b0aabdabeba011c8e1a80156bfe83b
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-02-28 23:06:16 -0600

    Se modifican rutas

--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { addCompany, getCompany, getCompanyCategory, getCompanyYears, getExcelCompanies } from "./company.controller.js"
+import { addCompany, getCompany, getCompanyCategory, getCompanyOrder, getCompanyYears, getExcelCompanies } from "./company.controller.js"
 import { validateJwt } from "../../middlewares/validate.js"
 
 const api = Router()
@@ -7,8 +7,9 @@ const api = Router()
 api.post('/addCompany',[validateJwt],addCompany)
 api.get('/getCompany',[validateJwt], getCompany)
 
-api.get('/getExcel', getExcelCompanies)
-api.get('/getCompaniesByCategory', getCompanyCategory)
-api.get('/getTrayectoryYears', getCompanyYears)
+api.get('/getExcel', [validateJwt],getExcelCompanies)
+api.get('/getCompaniesByCategory',[validateJwt], getCompanyCategory)
+api.get('/getTrayectoryYears', [validateJwt],getCompanyYears)
+api.get('/getCompanyOrder', [validateJwt],getCompanyOrder)
 
 export default api
