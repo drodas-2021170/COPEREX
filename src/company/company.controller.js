a89961892220c08b05344791a8aa97cb6b347152
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-02-28 23:05:44 -0600

    Se modifica la función getExcelCompanies y getCompanyOrder

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -1,7 +1,8 @@
 import Company from "./company.model.js"
 import Category from "../category/category.model.js"
 import XlsxPopulate from "xlsx-populate"
-
+import path from 'path'
+import fs  from 'fs'
 
 export const addCompany = async(req,res) =>{
     try {
@@ -64,15 +65,12 @@ export const getCompanyCategory = async(req,res) =>{
 export const getCompanyOrder= async(req,res) =>{
     try {
         let {Order} = req.body
-        
-        if(Order === 1){
-            let companyCategory  = await Company.find().asc()
-    
+        if(Order === 'A-Z'){
+            let companyCategory  = await Company.find().sort({name:1})
             if(companyCategory.length === 0) return res.status(404).send({success: false, message:'No companies found for this category'})
                 return res.send({success: true, message: `Companies found for category: ${companyCategory.category}`, companyCategory, total: companyCategory.length})
-        }else if(Order === 1){
-            let companyCategory  = await Company.find().dsc()
-    
+        }else if(Order === 'Z-A'){
+            let companyCategory  = await Company.find().sort({name:-1})
             if(companyCategory.length === 0) return res.status(404).send({success: false, message:'No companies found for this category'})
                 return res.send({success: true, message: `Companies found for category: ${companyCategory.category}`, companyCategory, total: companyCategory.length})
         }else{
@@ -87,6 +85,8 @@ export const getCompanyOrder= async(req,res) =>{
 export const getExcelCompanies = async(req,res) =>{
     try {
         const excel = await XlsxPopulate.fromBlankAsync()
+        const folderPath = path.join('Report')
+        const xlsx = '.xlsx'
         let company = await Company.find().populate("category", "name");
 
         const data = company.map(company => [
@@ -110,7 +110,14 @@ export const getExcelCompanies = async(req,res) =>{
         excel.sheet(0).column('E').width(30);
         excel.sheet(0).column('F').width(20);
 
-        excel.toFileAsync("./companies2.xlsx")
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath);
+        }
+
+        const filePath = path.join(folderPath, 'companiesIn' + Date.now() +'.xlsx');
+        await excel.toFileAsync(filePath);
+        
+
         return res.send({success:true, message:'Generating Excel'})
     } catch (err) {
         console.error(err)
