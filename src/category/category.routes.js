620853c975b4ba230bd9f3849f259ae1872f2406
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-02 18:12:19 -0600

    Se agregan validaciones a las rutas

--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -1,8 +1,9 @@
 import {Router} from 'express'
 import { validateJwt } from '../../middlewares/validate.js'
 import { addCategory } from './category.controller.js'
+import { addCetegoryValidator } from '../../middlewares/validation.js'
 
 const api = Router()
 
-api.post('/addCategory', [validateJwt], addCategory)
+api.post('/addCategory', [validateJwt, addCetegoryValidator], addCategory)
 export default api
