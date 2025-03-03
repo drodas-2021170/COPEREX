27b70c2b3788ab2c06baac5393b4e486dbbe4b02
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-02 18:09:36 -0600

    Se modifica la función validateJwt

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -2,18 +2,16 @@
 
 import jwt from 'jsonwebtoken'
 
-export const validateJwt = async(req,res, next) =>{
+export const validateJwt = async(req, res, next) =>{
     try {
         let secretKey = process.env.SECRET_KEY
-        let{authorization} = req.headers
-
-        if(!authorization) return res.status(401).send({success:false, message:'You most be loggoed in'})
-        
-        let user = jwt.verify(authorization, secretKey)
+        let { authorization } = req.headers
+        if(!authorization) return res.status(401).send({success: false, message: 'You must be logged in'})
+        let user = jwt.verify(authorization,secretKey)
         req.user = user
         next()
-    } catch (err) {
+        } catch (err) {
         console.error(err)
-        return res.status(500).send({success:false, message:'General Error', err})
+        return res.status(401).send({success: false, message: 'Invalid Token'})
     }
 }
